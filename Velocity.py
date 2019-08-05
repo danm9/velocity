26ee835eb3247ee420bdbd13bdc586c5d8db56a5
danm9 <44756478+danm9@users.noreply.github.com>
2019-08-05 04:38:48 -0400

    Update Velocity.py

--- Velocity.py
+++ Velocity.py
@@ -1,15 +1,16 @@
 velocity = input("Current Velocity: ")
 #print(velocity)
 total_team_members = input("Total Team Members: ")
-total_days_out_per_sprint = input("Total Days Out: ")
+total_days_out = input("Total Days Out: ")
 total_days_in_sprint = input("Total Days in Sprint: ")
 average_injections = input("Average Injections Per Sprint: ")
-total_days_in_sprint_for_team = int(total_team_members) * int(total_days_in_sprint)
-#print(total_days_in_sprint_for_team)
-velocity_per_person_per_day = int(velocity) / int(total_days_in_sprint_for_team)
+deploys = input("How Many Deploys for This Sprint: ")
+deploy_time = int(deploys) * 2
+total_hours_out_per_sprint = int(total_days_out) * 6 + int(deploy_time)
+total_days_out_per_sprint = int(total_hours_out_per_sprint) / 6
+velocity_per_person_per_day = int(velocity) / (int(total_days_in_sprint) * int(total_team_members))
 #print(velocity_per_person_per_day)
 plan_for = int(velocity) - (int(total_days_out_per_sprint) * float(velocity_per_person_per_day)) - (int(average_injections) * 2)
-#print(plan_for)
 print("Plan For " + str(round(plan_for)))
 
 close_check = input("Are you done?: ")
