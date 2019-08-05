b4215d73d1798dc0c0e9c05e89c89d919e7715e8
danm9 <44756478+danm9@users.noreply.github.com>
2019-08-05 04:40:33 -0400

    Update Velocity.py

--- Velocity.py
+++ Velocity.py
@@ -1,5 +1,5 @@
+#Using inputs to to run this on local machine quickly
 velocity = input("Current Velocity: ")
-#print(velocity)
 total_team_members = input("Total Team Members: ")
 total_days_out = input("Total Days Out: ")
 total_days_in_sprint = input("Total Days in Sprint: ")
@@ -9,12 +9,12 @@ deploy_time = int(deploys) * 2
 total_hours_out_per_sprint = int(total_days_out) * 6 + int(deploy_time)
 total_days_out_per_sprint = int(total_hours_out_per_sprint) / 6
 velocity_per_person_per_day = int(velocity) / (int(total_days_in_sprint) * int(total_team_members))
-#print(velocity_per_person_per_day)
 plan_for = int(velocity) - (int(total_days_out_per_sprint) * float(velocity_per_person_per_day)) - (int(average_injections) * 2)
 print("Plan For " + str(round(plan_for)))
 
+#running this check to keep the window open longer
 close_check = input("Are you done?: ")
 if close_check == 'yes':
-    print("Thanks Boss")
+    print("Good")
 else: 
-    print("Tough")
+    print("Open it Again")
